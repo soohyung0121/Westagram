e62ab502bcef67fc20f61867fca136f32f63e689
김수형 <soohyung0121@gmail.com>
2020-04-10 22:15:33 +0900

    Westagram codereview

--- westa/comment/views.py
+++ westa/comment/views.py
@@ -1,24 +1,27 @@
 import json
 
 from django.views import View
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from .models import Comment
 
 class CommentView(View):
     def post(self, request):
-        data = json.loads(request.body)
+        try:
+            data = json.loads(request.body)
+            Comment(
+                comment_user    = data['comment_user'],
+                comment_text    = data['comment_text'],
+            ).save()
 
-        Comment(
-            comment_user         = data['comment_user'],
-            comment_text         = data['comment_text'],
-        ).save()
+            return HttpResponse(status = 200)
 
-        return JsonResponse({"message" : "good"}, status = 200)
+        except KeyError:
+            return JsonResponse({"message" : "INVALID ERROR"}, status = 400)
+
 
     def get(self, request):
         data = json.loads(request.body)
-
         fix_user_text = []
         comment_context = Comment.objects.values()
 
@@ -26,4 +29,4 @@ class CommentView(View):
             if comment_context[i]['comment_user'] == data['comment_user']:
                 fix_user_text.append(comment_context[i]['comment_text'])
 
-        return JsonResponse({'soo' : fix_user_text}, status = 200)
+        return JsonResponse({data['comment_user'] : fix_user_text}, status = 200)
--- westa/user/views.py
+++ westa/user/views.py
@@ -6,18 +6,23 @@ from .models import User
 class CreateView(View):
     def post(self, request):
         data = json.loads(request.body)
-        User(
-            user_id     = data['user_id'],
-            email       = data['email'],
-            password    = data['password'],
-        )
+        try:
+            user = User(
+                user_id     = data['user_id'],          # user_name 으로 이름 변경해야 함, id는 숫자같은 느낌이 남
+                email       = data['email'],
+                password    = data['password'],
+            )
 
-        if User.objects.filter(user_id = data['user_id']).exists() == True:
-            return JsonResponse({"message" : "이미 존재하는 아이디입니다."}, status = 401)
+            # if User.objects.filter(user_id=data['user_id']).exists() == True: #이미 == 전까지 트루임, 굳이 없어도 됨
+            if User.objects.filter(user_id = data['user_id']).exists() :
+                return JsonResponse({"message" : "이미 존재하는 아이디입니다."}, status = 401)
 
-        else:
-            User.objects.create(user_id = data['user_id'], email = data['email'], password = data['password'])
-            return JsonResponse({"message" : "회원으로 가입되셨습니다."}, status = 200)
+            else:
+                # User.objects.create(user_id = data['user_id'], email = data['email'], password = data['password'])
+                user.save()
+                return JsonResponse({"message" : "회원으로 가입되셨습니다."}, status = 200)
+        except KeyError:
+            return JsonResponse({'message' : "INVALID ERROR"}, status = 400)
 
     def get(self, request):
         users = User.objects.values()
@@ -26,13 +31,8 @@ class CreateView(View):
 class LoginView(View):
     def post(self, request):
         data = json.loads(request.body)
-        User(
-            user_id     = data['user_id'],
-            email       = data['email'],
-            password    = data['password']
-        )
 
-        if User.objects.filter(user_id = data['user_id'], password = data['password']).exists() == True :
+        if User.objects.filter(user_id = data['user_id'], password = data['password']).exists() : #user_id, password and 처리가 된다고 합니다.
             return JsonResponse({"message": "로그인에 성공하셨습니다."}, status = 200)
         else:
             return JsonResponse({"message" : "아이디나 비밀번호가 일치하지 않습니다."}, status = 401)
